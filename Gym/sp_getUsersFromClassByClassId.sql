26e6cd66e5091d348b5c02a593837103a1012835
A-rielox <arielox.ag@gmail.com>
2024-03-26 19:37:08 -0600

    sp

---
+++ Gym/sp_getUsersFromClassByClassId.sql
@@ -0,0 +1,54 @@
+USE Gym;
+GO
+
+DROP PROCEDURE IF EXISTS dbo.sp_getUsersFromClassByClassId;
+GO
+
+CREATE PROCEDURE dbo.sp_getUsersFromClassByClassId
+(
+	@userType INT		-- 2 member, 3 profe, NULL ambos
+	, @claseId INT
+)
+AS
+
+SET NOCOUNT ON;
+SET XACT_ABORT ON;
+
+BEGIN
+
+	SELECT au.Id, au.UserName, au.KnownAs, pic.url
+	FROM [UserClase] uc
+	LEFT JOIN [UserRoles] ur ON uc.userId = ur.userId
+	LEFT JOIN [AppUsers] au ON uc.userId = au.Id
+	LEFT JOIN [Pictures] pic ON uc.userId = pic.appUserId
+	WHERE	
+			uc.claseId = @claseId
+			AND (						-- si es NULL => se salta este AND
+				ISNULL(@userType, 0) = 0
+					OR ur.roleId = @userType
+				)
+			AND ( pic.isMain = 1 OR pic.isMain IS NULL )
+	ORDER BY au.UserName	
+;
+
+END
+GO
+
+
+
+EXEC dbo.sp_getUsersFromClassByClassId @userType = null		-- 2 member, 3 profe, NULL ambos
+								, @claseId = 1;		-- 1 salsa ppte
+
+/*
+	SELECT au.UserName, au.KnownAs, Pic.url
+	FROM [UserClase] uc
+	LEFT JOIN [UserRoles] ur ON uc.userId = ur.userId
+	LEFT JOIN [AppUsers] au ON uc.userId = au.Id
+	OUTER APPLY (
+		SELECT * FROM [Pictures] pict
+		WHERE uc.userId = pict.appUserId
+	) AS Pic
+	WHERE	ur.roleId = 3
+			AND uc.claseId = 1
+	ORDER BY au.UserName	
+*/
